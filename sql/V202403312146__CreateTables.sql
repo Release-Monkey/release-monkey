bf84fb402efa9510d217014f1a8188e14002f9d0
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-03-31 21:58:22 +0200

    Added create sqls

---
+++ sql/V202403312146__CreateTables.sql
@@ -0,0 +1,97 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/TszF50
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [User] (
+    [UserID] int  NOT NULL ,
+    [Name] varchar(200)  NOT NULL ,
+    [EmailAddress] varchar(200)  NOT NULL ,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED (
+        [UserID] ASC
+    )
+)
+
+CREATE TABLE [Project] (
+    [ProjectID] int  NOT NULL ,
+    [ProjectName] varchar(200)  NOT NULL ,
+    [Repo] varchar(200)  NOT NULL ,
+    [Token] varchar(200)  NOT NULL ,
+    CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED (
+        [ProjectID] ASC
+    ),
+    CONSTRAINT [UK_Project_ProjectName] UNIQUE (
+        [ProjectName]
+    ),
+    CONSTRAINT [UK_Project_Repo] UNIQUE (
+        [Repo]
+    ),
+    CONSTRAINT [UK_Project_Token] UNIQUE (
+        [Token]
+    )
+)
+
+CREATE TABLE [UserProject] (
+    [UserProjectID] int  NOT NULL ,
+    [UserID] int  NOT NULL ,
+    [ProjectID] int  NOT NULL ,
+    [Role] int  NOT NULL ,
+    CONSTRAINT [PK_UserProject] PRIMARY KEY CLUSTERED (
+        [UserProjectID] ASC
+    )
+)
+
+CREATE TABLE [Release] (
+    [ReleaseID] int  NOT NULL ,
+    [ReleaseName] varchar(200)  NOT NULL ,
+    [ProjectID] int  NOT NULL ,
+    [State] int  NOT NULL ,
+    CONSTRAINT [PK_Release] PRIMARY KEY CLUSTERED (
+        [ReleaseID] ASC
+    )
+)
+
+CREATE TABLE [ReleaseTester] (
+    [ReleaseTesterID] int  NOT NULL ,
+    [ReleaseID] int  NOT NULL ,
+    [TesterID] int  NOT NULL ,
+    [State] int  NOT NULL ,
+    [Comment] varchar(200)  NOT NULL ,
+    CONSTRAINT [PK_ReleaseTester] PRIMARY KEY CLUSTERED (
+        [ReleaseTesterID] ASC
+    )
+)
+
+ALTER TABLE [UserProject] WITH CHECK ADD CONSTRAINT [FK_UserProject_UserID] FOREIGN KEY([UserID])
+REFERENCES [User] ([UserID])
+
+ALTER TABLE [UserProject] CHECK CONSTRAINT [FK_UserProject_UserID]
+
+ALTER TABLE [UserProject] WITH CHECK ADD CONSTRAINT [FK_UserProject_ProjectID] FOREIGN KEY([ProjectID])
+REFERENCES [Project] ([ProjectID])
+
+ALTER TABLE [UserProject] CHECK CONSTRAINT [FK_UserProject_ProjectID]
+
+ALTER TABLE [Release] WITH CHECK ADD CONSTRAINT [FK_Release_ProjectID] FOREIGN KEY([ProjectID])
+REFERENCES [Project] ([ProjectID])
+
+ALTER TABLE [Release] CHECK CONSTRAINT [FK_Release_ProjectID]
+
+ALTER TABLE [ReleaseTester] WITH CHECK ADD CONSTRAINT [FK_ReleaseTester_ReleaseID] FOREIGN KEY([ReleaseID])
+REFERENCES [Release] ([ReleaseID])
+
+ALTER TABLE [ReleaseTester] CHECK CONSTRAINT [FK_ReleaseTester_ReleaseID]
+
+ALTER TABLE [ReleaseTester] WITH CHECK ADD CONSTRAINT [FK_ReleaseTester_TesterID] FOREIGN KEY([TesterID])
+REFERENCES [User] ([UserID])
+
+ALTER TABLE [ReleaseTester] CHECK CONSTRAINT [FK_ReleaseTester_TesterID]
+
+COMMIT TRANSACTION QUICKDBD
