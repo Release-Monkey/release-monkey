40d2112cb9a8f786cf4c5c99f3b19672a7d565be
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-03-31 22:08:54 +0200

    Added db usage

--- sql/V202403312146__CreateTables.sql
+++ sql/V202403312146__CreateTables.sql
@@ -1,12 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/TszF50
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-USE release_monkey_db;
+﻿USE release_monkey_db;
 
 BEGIN TRANSACTION QUICKDBD
 
