82c34ea88e75840c16d45888cd10a7f452188197
Abdul Gany Osman <abdul.osman@bbd.co.za>
2024-04-04 21:16:22 +0200

    Added ReleaseTester Repo and bugfix

--- server/src/Repositories/Db.cs
+++ server/src/Repositories/Db.cs
@@ -12,7 +12,6 @@ namespace ReleaseMonkey.src.Repositories
         public Db()
         {
             DotNetEnv.Env.Load();
-            string s = Environment.GetEnvironmentVariable("DB_HOST");
             SqlConnectionStringBuilder builder = new()
             {
                 DataSource = Environment.GetEnvironmentVariable("DB_HOST"),
