f619879444c440007776e4a153cde80d0b6d4773
Bryce <bryce.grahn@bbd.co.za>
2024-04-07 02:49:35 +0200

    insert testers and releasers into the user projects table

---
+++ server/src/Repositories/UserProjectsRepository.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using ReleaseMonkey.Server.Models;
+using ReleaseMonkey.src.Repositories;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ReleaseMonkey.Server.Repositories
+{
+  public class UserProjectsRepository
+  {
+    public List<UserProject> GetTestersForProject(SqlTransaction transaction, Db db, int projectId)
+    {
+      List<UserProject> userProjects = new List<UserProject>();
+      string sql = @"SELECT UserProjectID, UserID, ProjectID, Role FROM [UserProject] WHERE ProjectID=@ProjectID AND (Role=1 OR Role=2)";
+      using SqlCommand command = new(sql, db.Connection, transaction);
+      command.Parameters.Add("@ProjectID", SqlDbType.Int).Value = projectId;
+
+      using SqlDataReader reader = db.ExecuteReader(command);
+
+      while (reader.Read())
+      {
+        userProjects.Add(new UserProject(reader.GetInt32("UserProjectID"), reader.GetInt32("UserID"), reader.GetInt32("ProjectID"), reader.GetInt32("Role")));
+      }
+      return userProjects;
+    }
+
+    public List<UserProject> GetTestersForProject(Db db, int projectId)
+    {
+      List<UserProject> userProjects = new List<UserProject>();
+      string sql = @"SELECT UserProjectID, UserID, ProjectID, Role FROM [UserProject] WHERE ProjectID=@ProjectID AND (Role=1 OR Role=2)";
+      using SqlCommand command = new(sql, db.Connection);
+      command.Parameters.Add("@ProjectID", SqlDbType.Int).Value = projectId;
+
+      using SqlDataReader reader = db.ExecuteReader(command);
+
+      while (reader.Read())
+      {
+        userProjects.Add(new UserProject(reader.GetInt32("UserProjectID"), reader.GetInt32("UserID"), reader.GetInt32("ProjectID"), reader.GetInt32("Role")));
+      }
+      return userProjects;
+    }
+
+    public UserProject GetUserProjectById(Db db, int userProjectId)
+    {
+      string sql = @"SELECT UserProjectID, UserID, ProjectID, Role FROM [UserProject] WHERE UserProjectID=@UserProjectID";
+      using SqlCommand command = new(sql, db.Connection);
+      command.Parameters.Add("@UserProjectID", SqlDbType.Int).Value = userProjectId;
+
+      using SqlDataReader reader = db.ExecuteReader(command);
+
+      if (reader.Read())
+      {
+        return new UserProject(reader.GetInt32("UserProjectID"), reader.GetInt32("UserID"), reader.GetInt32("ProjectID"), reader.GetInt32("Role"));
+      }
+      else
+      {
+        throw new KeyNotFoundException($"There is no such user project with id {userProjectId}.");
+      }
+    }
+
+    public UserProject InsertUserProject(SqlTransaction transaction, Db db, int userId, int projectID, int role)
+    {
+      string sql = @" INSERT INTO[UserProject](UserId, ProjectID, Role)
+                            VALUES(@UserId, @ProjectID, @Role);
+                            SELECT SCOPE_IDENTITY();";
+
+      using (SqlCommand command = new(sql, db.Connection, transaction))
+      {
+        command.Parameters.Add("@UserId", System.Data.SqlDbType.Int).Value = userId;
+        command.Parameters.Add("@Role", System.Data.SqlDbType.Int).Value = role;
+        command.Parameters.Add("@ProjectID", System.Data.SqlDbType.Int).Value = projectID;
+
+        int userProjectId = (int)(decimal)command.ExecuteScalar();
+        return new UserProject(userProjectId, userId, projectID, role);
+
+      }
+    }
+
+    public List<int> GetUserIdsWithRole(Db db, int role, int projectId)
+    {
+      string sql = @"SELECT UserID FROM UserProject
+                            WHERE Role=@Role AND ProjectID=@ProjectID";
+
+      using SqlCommand command = new(sql, db.Connection);
+      command.Parameters.Add("@Role", SqlDbType.Int).Value = role;
+      command.Parameters.Add("@ProjectID", SqlDbType.Int).Value = projectId;
+
+      List<int> output = [];
+      using SqlDataReader reader = db.ExecuteReader(command);
+      while (reader.Read())
+      {
+        output.Add(reader.GetInt32("UserID"));
+      }
+
+      return output;
+    }
+
+  }
+}
+
---
+++ server/src/Services/UserProjectsService.cs
@@ -0,0 +1,57 @@
+using Microsoft.Data.SqlClient;
+using ReleaseMonkey.Server.Models;
+using ReleaseMonkey.Server.Repositories;
+using ReleaseMonkey.src.Common;
+using ReleaseMonkey.src.Repositories;
+using System.Transactions;
+
+namespace ReleaseMonkey.Server.Services
+{
+  public class UserProjectsService(UserProjectsRepository userProjects, UsersRepository users, Db db)
+  {
+
+    public Task<UserProject> GetUserProjectById(int userProjectId)
+    {
+      return Task.FromResult(userProjects.GetUserProjectById(db, userProjectId));
+    }
+
+    public Task<UserProject> InsertUserProjectByUserID(int userId, int projectId, int role)
+    {
+      using (SqlTransaction transaction = db.Connection.BeginTransaction())
+      {
+        try
+        {
+          UserProject userProject = userProjects.InsertUserProject(transaction, db, userId, projectId, role);
+          transaction.Commit();
+          return Task.FromResult(userProject);
+        }
+        catch (Exception)
+        {
+          transaction.Rollback();
+          throw;
+        }
+      }
+    }
+
+    public Task<UserProject> InsertUserProjectByEmail(string email, int projectId, int role)
+    {
+
+      User user = users.FindByEmail(email);
+
+      using (SqlTransaction transaction = db.Connection.BeginTransaction())
+      {
+        try
+        {
+          UserProject userProject = userProjects.InsertUserProject(transaction, db, user.Id, projectId, role);
+          transaction.Commit();
+          return Task.FromResult(userProject);
+        }
+        catch (Exception)
+        {
+          transaction.Rollback();
+          throw;
+        }
+      }
+    }
+  }
+}
