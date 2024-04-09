5e6f8ed678b5edd0226be1279f545370b60cc2cf
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-04-09 11:53:27 +0200

    Prelease going live integration

--- cli/Assembly.cs
+++ cli/Assembly.cs
@@ -44,7 +44,7 @@ namespace cli
                 return GetBuild() switch
                 {
                     Build.Developer => "http://localhost:3000",
-                    Build.Production => "http://52.210.18.60:3000",
+                    Build.Production => "http://52.210.18.60:5000",
                     _ => "http://52.210.18.60:3000"
                 };
 
